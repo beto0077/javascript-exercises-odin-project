2edba90cd03f2f981ff5951cf68f26d689c3347e
beto0077 <131410367+beto0077@users.noreply.github.com>
2024-04-09 17:52:22 -0600

    Exercise 2 (Exercise 02 - repeatString) completed and reviewed. The code in the .js file was modified as requested in the README file.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (word, number) {
+    let newString = ``;
+    if (number >= 0) {
+        for (let index = 0; index < number; index++) {
+            newString = newString.concat(word);
+        }
+    } else {
+        newString = `ERROR`;
+    }
+    return newString;
 };
 
 // Do not edit below this line
