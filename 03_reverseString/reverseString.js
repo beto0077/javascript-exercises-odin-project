70fe36ee15c2eb7bc68fb54cbfc6e3cf8de033da
beto0077 <131410367+beto0077@users.noreply.github.com>
2024-04-09 18:13:28 -0600

    Exercise 3 (Exercise 03 - Reverse a String) completed and reviewed. The code in the .js file was modified as requested in the README file.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversedWord = ``;
+    const myArr = word.split("");
+    for (let index = myArr.length - 1; index >= 0; index--) {
+        reversedWord = reversedWord + myArr[index];
+        
+    }
+    return reversedWord;
 };
 
 // Do not edit below this line
