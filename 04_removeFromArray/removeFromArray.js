e3becbd9ac9e0ce4a6b6ae12eefcae093f8bed71
beto0077 <131410367+beto0077@users.noreply.github.com>
2024-04-09 21:25:38 -0600

    Exercise 4 (Exercise 04 - removeFromArray) completed and reviewed. The code in the .js file was modified as requested in the README file.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,41 @@
-const removeFromArray = function() {
+//Version #1
+/*const removeFromArray = function (listWN, ...extras) {
+    const altArray = listWN;
+    console.log(extras);
+    console.log(listWN);
+    console.log("NEW conut");
 
+    for (let index = 0; index < altArray.length; index++) {
+        const element = altArray[index];
+        console.log("Element " + element);
+        for (let count = 0; count < extras.length; count++) {
+            let twoCups = element + " " + extras[count];
+            console.log(twoCups);
+            if (element === extras[count]) {
+                let position = listWN.lastIndexOf(extras[count]);
+                listWN.splice(position, 1);
+            }
+        }
+    }
+    console.log(altArray);
+};*/
+
+//Version #2 (I decided to rewrite the code after finding some problems with the first version.)
+const removeFromArray = function (array, ...elementsToRemove) {
+    const newArray = [...array]; // Create a shallow copy of the original array
+
+    for (let i = newArray.length - 1; i >= 0; i--) {
+        const currentElement = newArray[i];
+
+        for (let j = 0; j < elementsToRemove.length; j++) {
+            if (currentElement === elementsToRemove[j]) {
+                newArray.splice(i, 1); // Remove the element at the current index
+                break; // Exit the loop after removing the element
+            }
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
