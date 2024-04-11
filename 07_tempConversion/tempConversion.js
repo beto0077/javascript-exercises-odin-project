7140595ddaf73af0bb9b762926401545277ddf0d
beto0077 <131410367+beto0077@users.noreply.github.com>
2024-04-10 20:41:52 -0600

    Exercise 7 (Exercise 07 - tempConversion) completed and reviewed. The code in the .js file was modified as requested in the README file.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  return Number(((temperature - 32) * (5/9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  return Number(((temperature * 9.0 / 5.0) + 32.0).toFixed(1));
 };
 
 // Do not edit below this line
