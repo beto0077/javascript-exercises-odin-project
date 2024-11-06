9911ad4a01ce572bfc79990ec4357c532393519b
beto0077 <131410367+beto0077@users.noreply.github.com>
2024-11-06 17:23:10 -0600

    Exercise 10 (Exercise 10 - Palindromes) completed and reviewed. The code in the .js file was modified as requested in the README file. The solution to the code was carried out using the unusual logic of starting the Fibonacci sequence with 1 as the first number, the usual way that can be found on the internet starts with 0.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0)
+        return "OOPS";
+    if (num == 0)
+        return 0;
+    if (num == 1)
+        return 1;
+    if (num == 2)
+        return 1;
+    let num1 = 1;
+    let num2 = 1;
+    let sum;
+    let i = 2;
+    while (i < num) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+        i += 1;
+    }
+    return num2;
 };
 
 // Do not edit below this line
