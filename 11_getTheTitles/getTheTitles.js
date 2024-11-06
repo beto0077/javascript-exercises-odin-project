cb83cfd796f8408eab51b49342ced26f20f7db2f
beto0077 <131410367+beto0077@users.noreply.github.com>
2024-11-06 17:46:39 -0600

    Exercise 11 (Exercise 11 - Get the Titles!) completed and reviewed. The code in the .js file was modified as requested in the README file.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titlesList = books.map(book => book.title);
+    return titlesList;
 };
 
 // Do not edit below this line
